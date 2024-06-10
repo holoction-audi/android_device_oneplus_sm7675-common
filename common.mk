c7d1996601d8fe236315ace7240ce82530098218
Bruno Martins <bgcngm@gmail.com>
2024-06-10 19:51:18 +0530

    sm8650-common: Migrate to common libqti-perfd-client and power-libperfmgr
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -306,10 +306,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/pineapple/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    android.hardware.power@1.2.vendor \
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
@@ -349,7 +348,11 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
+    hardware/qcom-caf/common/libqti-perfd-client \
     vendor/gapps/arm64
 
 # Storage
