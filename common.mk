a1d6be1e794c26b4935db79be8e8daffe4e4bfff
Adhitya Mohan <me@adhityamohan.in>
2024-08-31 22:21:49 +0200

    sm8650-common: Build USB service as aidl too  * should have done this together

--- common.mk
+++ common.mk
@@ -541,8 +541,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget-service-qti \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
     libusbhost.vendor
 
 PRODUCT_SOONG_NAMESPACES += \
