06d5bf7ca68949a3b177800276641ec374142ece
John Galt <johngaltfirstrun@gmail.com>
2024-10-02 14:42:49 -0400

    extract-files: fix qsap* seccomp failures

--- extract-files.sh
+++ extract-files.sh
@@ -100,7 +100,7 @@ function blob_fixup() {
         vendor/lib64/vendor.libdpmframework.so)
             grep -q libhidlbase_shim.so "$2" || "$PATCHELF" --add-needed libhidlbase_shim.so "$2"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy|vendor/etc/seccomp_policy/qsap_sensors.policy|vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy)
             grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
             ;;
         vendor/lib64/libqcodec2_core.so)
