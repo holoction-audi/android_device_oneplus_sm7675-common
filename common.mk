674bcf5f9a89b5a4e9648e92b490fa6b5827861e
Bruno Martins <bgcngm@gmail.com>
2024-10-04 22:26:51 +0100

    sm7675-common: Switch to AIDL fingerprint HAL
    Change-Id: I1e95e82969b20917ab242087a4db43149bf3dd2d

--- common.mk
+++ common.mk
@@ -223,6 +223,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.fdsan=warn_once
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.oplus
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
