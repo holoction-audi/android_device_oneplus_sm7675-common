9e455dc9e96a0667ba679518c9b689d4dfd7bedf
John Galt <johngaltfirstrun@gmail.com>
2024-12-02 13:30:54 -0500

    BoardConfigCommon: inherit hardware/oplus sepolicy
    Fixed qcom sepolicy_vndr duplicate context to allow booting with
    hw/oplus sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -236,8 +236,8 @@ BOOT_SECURITY_PATCH := 2024-07-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
+include hardware/oplus/sepolicy/qti/SEPolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-#include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Touch
 SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_TOUCH_HAL
