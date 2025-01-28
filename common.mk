0af9ead17923dab1d0636c75af5786da63ef4e36
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-28 03:20:14 +0200

    sm8650-common: Switch to richtap vibrator service
    Change-Id: If0dd051ed582a19bb7b6ad7314ef67f2ab05a2f1

--- common.mk
+++ common.mk
@@ -607,7 +607,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.oplus
+    android.hardware.vibrator.service.oplus-richtap
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
