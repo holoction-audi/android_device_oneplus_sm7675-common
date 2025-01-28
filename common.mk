c8fe38cbfc50ada0657db20e80fdd0b61102229a
John Galt <johngaltfirstrun@gmail.com>
2025-01-28 09:36:11 -0500

    init: move init.qti.kernel.rc to tree

--- common.mk
+++ common.mk
@@ -260,7 +260,8 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/init/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
