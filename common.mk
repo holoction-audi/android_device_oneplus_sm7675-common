DqrKnzx/device_oneplus_sm8650-common

init: move init.kernel.post_boot-pineapple.sh to tree

common.mk
@@ -280,6 +280,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.oplus.rc \
     init.kernel.post_boot.sh \
+    init.kernel.post_boot-pineapple.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \

holoction-audi/android_device_oneplus_sm7675-common

common.mk
@@ -292,6 +292,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.oplus.rc \
     init.kernel.post_boot.sh \
+    init.kernel.post_boot-cliffs.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \