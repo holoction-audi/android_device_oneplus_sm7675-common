62208987ea1c1f6f6cc09d972f27f57c93c69a30
John Galt <johngaltfirstrun@gmail.com>
2025-06-08 08:30:41 -0400

    sm8650-common: drop lmkd tweaks
    We now use SLMK

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,6 @@ TARGET_CAMERA_PACKAGE_NAME := com.oplus.camera
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
