cd3cc5159a4d8656ebe0997cba6772eaf12edf1a
kok-o3 <ko282ko82@gmail.com>
2025-07-14 20:38:43 +0530

    sm7675-common: Switch to stock PowerHAL service
     -QTI PowerHAL doesn't work with perf2 for some reason.
    
    Signed-off-by: kok-o3 <ko282ko82@gmail.com>

--- common.mk
+++ common.mk
@@ -302,9 +302,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/pineapple/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
