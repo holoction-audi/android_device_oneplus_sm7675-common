be6b7a9f1fcc2b321d16e098d7f2320416770afe
dianlujitao <dianlujitao@lineageos.org>
2025-08-05 20:57:22 +0800

    sm7675-common: Enable OMAPI service
     * Import hal_uuid_map_config.xml from hardware/nxp/keymint/KM300/res
       and adjust UID to match QCOM definitions
    
    Change-Id: I393ff4053bd0e0f2b0ebccb83b03c7a1fdffb598

--- common.mk
+++ common.mk
@@ -331,6 +331,20 @@ PRODUCT_PACKAGES += \
 # QCOM
 TARGET_BOARD_PLATFORM := pineapple
 
+# SecureElement
+PRODUCT_PACKAGES += \
+    SecureElementResTarget_Vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.android.se.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.se.xml \
+    $(LOCAL_PATH)/configs/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_23.xml \
+    $(LOCAL_PATH)/configs/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_29.xml \
+    $(LOCAL_PATH)/configs/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_31.xml \
+    $(LOCAL_PATH)/configs/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_config.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
