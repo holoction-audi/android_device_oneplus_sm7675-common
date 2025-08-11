a65b9bcfb4697cc454da8dfa44e17ad6fd285d1d
kok-o3 <ko282ko82@gmail.com>
2025-08-11 19:17:35 +0530

    sm7675-common: Load system_dlkm.modules.blocklist from kernel
    Signed-off-by: kok-o3 <ko282ko82@gmail.com>

--- common.mk
+++ common.mk
@@ -225,7 +225,7 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 
 # Kernel
  PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system_dlkm.modules.blocklist:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
+    kernel/oneplus/sm8650/modules.systemdlkm_blocklist.msm.pineapple:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
 
 # Keymint
 PRODUCT_PACKAGES += \
