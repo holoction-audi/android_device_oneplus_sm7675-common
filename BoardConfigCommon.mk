e99cddbd640b7e4ae232cc1fb0b0601a5c752dc8
DillerOFire <niktofe1@gmail.com>
2025-09-17 16:04:08 +1000

    Revert "squash switch to prebuilt kernel"
    This reverts commit 561b788ad8fbe48470ab3d8f4b1c795161920648.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,10 +62,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 # DTB / DTBO
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-PREBUILT_DTB_DTBO_DIR := device/oneplus/audi-prebuilt/images
-BOARD_PREBUILT_DTBIMAGE_DIR := $(PREBUILT_DTB_DTBO_DIR)/dtbs/
-BOARD_PREBUILT_DTBOIMAGE := $(wildcard $(PREBUILT_DTB_DTBO_DIR)/dtbo.img)
-BOARD_RAMDISK_USE_LZ4 := true
+BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Properties
@@ -99,61 +96,75 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 
-# KERNEL_LTO := none
+TARGET_KERNEL_SOURCE := kernel/oneplus/sm8650
 
-# TARGET_FORCE_PREBUILT_KERNEL := true
-# TARGET_KERNEL_SOURCE := kernel/oneplus/sm7675
-# TARGET_KERNEL_CONFIG := \
-#     gki_defconfig \
-#    vendor/pineapple_GKI.config \
-#    vendor/oplus/pineapple_GKI.config \
-#    vendor/debugfs.config
+TARGET_KERNEL_ADDITIONAL_FLAGS := CONFIG_OPLUS_DEVICE_DTBS=y
+TARGET_KERNEL_CONFIG := \
+    gki_defconfig \
+    vendor/pineapple_GKI.config \
+    vendor/oplus/pineapple_GKI.config
 
 # Kernel modules
-#BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
-#BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
-#BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
-#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
-#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot))
-#BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
-#BOOT_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery $(COMMON_PATH)/modules.include.vendor_ramdisk))
-#SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.include.system_dlkm))
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
+SYSTEM_KERNEL_MODULES := $(BOARD_SYSTEM_KERNEL_MODULES_LOAD)
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.pineapple $(COMMON_PATH)/modules.load.oplus))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot.pineapple $(COMMON_PATH)/modules.load.vendor_boot.oplus))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery.pineapple $(COMMON_PATH)/modules.load.recovery.oplus))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 
-#TARGET_KERNEL_EXT_MODULE_ROOT := kernel/oneplus/sm7675-modules
-#TARGET_KERNEL_EXT_MODULES := \
-#	qcom/opensource/mmrm-driver \
-#	qcom/opensource/mm-drivers/hw_fence \
-#	qcom/opensource/mm-drivers/msm_ext_display \
-#	qcom/opensource/mm-drivers/sync_fence \
-#	qcom/opensource/securemsm-kernel \
-#	qcom/opensource/audio-kernel \
-#	qcom/opensource/camera-kernel \
-#	qcom/opensource/dataipa/drivers/platform/msm \
-#	qcom/opensource/datarmnet-ext/mem \
-#	qcom/opensource/datarmnet/core \
-#	qcom/opensource/datarmnet-ext/aps \
-#	qcom/opensource/datarmnet-ext/offload \
-#	qcom/opensource/datarmnet-ext/shs \
-#	qcom/opensource/datarmnet-ext/perf \
-#	qcom/opensource/datarmnet-ext/perf_tether \
-#	qcom/opensource/datarmnet-ext/sch \
-#	qcom/opensource/datarmnet-ext/wlan \
-#	qcom/opensource/display-drivers/msm \
-#	qcom/opensource/synx-kernel \
-#	qcom/opensource/dsp-kernel \
-#	qcom/opensource/eva-kernel \
-#	qcom/opensource/video-driver \
-#	qcom/opensource/graphics-kernel \
-#	qcom/opensource/wlan/platform \
-#	qcom/opensource/wlan/qcacld-3.0 \
-#	qcom/opensource/bt-kernel \
-#	qcom/opensource/spu-kernel \
-#	qcom/opensource/mm-sys-kernel/ubwcp \
-#	qcom/opensource/fingerprint \
-#	qcom/opensource/touch-drivers \
-#	nxp/opensource/driver \
-#	st/opensource/driver \
-#	st/opensource/eSE-driver
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/oneplus/sm8650-modules
+TARGET_KERNEL_EXT_MODULES := \
+    qcom/opensource/mmrm-driver \
+    qcom/opensource/mm-drivers/hw_fence \
+    qcom/opensource/mm-drivers/msm_ext_display \
+    qcom/opensource/mm-drivers/sync_fence \
+    qcom/opensource/securemsm-kernel \
+    qcom/opensource/audio-kernel \
+    qcom/opensource/synx-kernel \
+    qcom/opensource/camera-kernel \
+    qcom/opensource/datarmnet-ext/mem \
+    qcom/opensource/dataipa/drivers/platform/msm \
+    qcom/opensource/datarmnet/core \
+    qcom/opensource/datarmnet-ext/aps \
+    qcom/opensource/datarmnet-ext/offload \
+    qcom/opensource/datarmnet-ext/shs \
+    qcom/opensource/datarmnet-ext/perf \
+    qcom/opensource/datarmnet-ext/perf_tether \
+    qcom/opensource/datarmnet-ext/sch \
+    qcom/opensource/datarmnet-ext/wlan \
+    qcom/opensource/display-drivers/msm \
+    qcom/opensource/dsp-kernel \
+    qcom/opensource/eva-kernel \
+    qcom/opensource/video-driver \
+    qcom/opensource/graphics-kernel \
+    qcom/opensource/wlan/platform \
+    qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
+    qcom/opensource/wlan/qcacld-3.0/.qca6750 \
+    qcom/opensource/bt-kernel \
+    qcom/opensource/spu-kernel \
+    qcom/opensource/mm-sys-kernel/ubwcp \
+    nxp/opensource/driver
+
+TARGET_KERNEL_EXT_MODULES += \
+    oplus/hardware/radio/kernel/mdmfeature:kbuild \
+    oplus/kernel/cpu/thermal:kbuild \
+    oplus/kernel/device_info/pogo_keyboard:kbuild \
+    oplus/kernel/device_info/tri_state_key:kbuild \
+    oplus/kernel/dfr:kbuild \
+    oplus/kernel/graphics:kbuild \
+    oplus/kernel/network/oplus_network_oem_qmi:kbuild \
+    oplus/kernel/network/oplus_network_esim:kbuild \
+    oplus/kernel/network/oplus_network_sim_detect:kbuild \
+    oplus/kernel/network/oplus_rf_cable_monitor:kbuild \
+    oplus/kernel/touchpanel/oplus_touchscreen_v2/touch_custom:kbuild \
+    oplus/kernel/touchpanel/oplus_touchscreen_v2:kbuild \
+    oplus/kernel/touchpanel/synaptics_hbp:kbuild \
+    oplus/secure/biometrics/fingerprints/bsp/uff/driver:kbuild \
+    oplus/secure/common/bsp/drivers/oplus_secure_common \
+    oplus/sensor/kernel/oplus_consumer_ir:kbuild \
+    oplus/sensor/kernel/qcom/sensor:kbuild
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
